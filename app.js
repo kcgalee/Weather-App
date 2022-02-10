7f1bb1afa4cfe69b3400c5cf5c8a5d14b9be586a
Kylie Lee <kcgalee@gmail.com>
2022-02-10 00:01:58 -0500

    Edit

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ let weather = {
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed);
         document.querySelector(".city").innerText = "Weather in "+ name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "° C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + humidity + "km/h";
     }
 };
 

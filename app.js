9e812997fb1443ed5378e0cf173ab4978fae425c
Kylie Lee <kcgalee@gmail.com>
2022-02-09 19:07:37 -0500

    Edits

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
-
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = '${proxy}https://api.openweathermap.org/data/2.5/weather?lat=$35&lon=139&appid=76ef2b7c390c27b11497cbde51f19a47';
-
-            fetch(api)
-                .then(response =>{
-                    return response.json();
-                })
-                .then(data =>{
-                    console.log(data);
-                });
-        });
-    }
-});
+let weather = {
+    "apiKey": "76ef2b7c390c27b11497cbde51f19a47"
+}

d5a2851c5ab738216abf1192733b440b9e4703e5
Kylie Lee <kcgalee@gmail.com>
2022-02-09 23:48:24 -0500

    Edit

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 let weather = {
-    "apiKey": "76ef2b7c390c27b11497cbde51f19a47"
-}
+    apiKey: "76ef2b7c390c27b11497cbde51f19a47",
+    // fetchWeather: function() {
+    //     fetch(
+    //         "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=76ef2b7c390c27b11497cbde51f19a47"
+    //     )
+    //     .then((response) => response.json())
+    //     .then((data)=>console.log(data));
+
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data)=>this.displayWeather(data));
+    },
+    displayWeather: function (data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in "+ name;
+    }
+};
+

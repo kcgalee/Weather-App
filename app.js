2bbc603c51b12d5b4612aad70af4cd0c5cd5bba3
Kylie Lee <kcgalee@gmail.com>
2022-02-10 19:29:55 -0500

    Edit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" +
             city +
-            "&units=metric&appid=" +
+            "&units=imperial&appid=" +
             this.apiKey
         )
         .then((response) => response.json())
@@ -22,13 +22,30 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed);
+        // console.log(name,icon,description,temp,humidity,speed); (get rid of after enter key)
         document.querySelector(".city").innerText = "Weather in "+ name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "° C";
+        document.querySelector(".temp").innerText = temp + "° F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + humidity + "km/h";
+    },
+    //gets content of search bar
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
+//search bar
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+
+//enter key
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Detroit");
